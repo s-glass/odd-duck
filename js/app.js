c3b819e227721901e9e70a3cf2fde47ecb9134e4
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-02-22 14:57:29 -0800

    lab 13

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
 let imgThree = document.getElementById('img-three');
 let resultsButton = document.getElementById('show-results-button');
+document.querySelector('h3').style.visibility = 'hidden';
 
 // ***** CANVAS ELEMENT FOR CHART *****
 let ctx = document.getElementById('my-chart');
@@ -29,8 +30,19 @@ function Item(name, fileExtension = 'jpg') {
 
 function randomIndex() {
   return Math.floor(Math.random() * itemArray.length);
-
+}
 function renderImg() {
+  // let imgOneIndex = randomIndex();
+  // let imgTwoIndex = randomIndex();
+  // let imgThreeIndex = randomIndex();
+
+  // while (imgOneIndex = imgTwoIndex){
+  //   imgTwoIndex = randomIndex();
+  // } if (imgOneIndex = imgThreeIndex){
+  //   imgThreeIndex = randomIndex();
+  // } else if (imgTwoIndex = imgThreeIndex){
+  //   imgThreeIndex = randomIndex();
+  // }
 
   while(photoArray.length < 6){
     let randNum = randomIndex();
@@ -53,7 +65,6 @@ function renderImg() {
   imgThree.src = itemArray[imgThreeIndex].image;
   imgThree.alt = itemArray[imgThreeIndex].name;
 
-  // TODO: increase the views on the images
   itemArray[imgOneIndex].views++;
   itemArray[imgTwoIndex].views++;
   itemArray[imgThreeIndex].views++;
@@ -72,6 +83,9 @@ for (let i = 0; i < itemArray.length; i++) {
   itemVotes.push(itemArray[i].votes);
   itemViews.push(itemArray[i].views);
 }
+
+Chart.defaults.font.size = 20; //eslint-disable-line
+Chart.defaults.font.weight = 'bold'; //eslint-disable-line
 
 let chartObj = {
   type: 'bar',
@@ -95,9 +109,9 @@ let chartObj = {
   options: {
     scales: {
       y: {
-        beginAtZero: true
+        beginAtZero: true,
       }
-    }
+    },
   }
 };
 
@@ -107,27 +121,30 @@ let chartObj = {
 // *** EVENT HANDLERS ***
 
 function handleImgClick(event) {
-  // TODO: Identify the image that was clicked
   let imgClicked = event.target.alt;
   console.dir(imgClicked);
 
-  // TODO: Increase the number of clicks on that image
   for (let i = 0; i < itemArray.length; i++) {
     if (imgClicked === itemArray[i].name) {
       itemArray[i].votes++;
+      votingRounds--;
+      renderImg();
     }
   }
 
-  // TODO: decrement the voting rounds
-  votingRounds--;
-
-  // TODO: Rerender of Imgs
-  renderImg();
-
-  // TODO: once votings are done - stop the click
   if (votingRounds === 0) {
     imgContainer.removeEventListener('click', handleImgClick);
-  }
+    // document.querySelector('h3').style.visibility = 'visible';
+
+// **** LOCAL STORAGE STARTS HERE *******
+// STEP 1 - CONVERT DATA TO STRING FOR LOCAL STORAG
+let stringifiedItems = JSON.stringify(itemArray);
+
+console.log('Stringified Items >>> ', stringifiedItems);
+
+// STEP 2 - Set stringified items into local storage
+localStorage.setItem('myItems', stringifiedItems);
+}
 }
 
 function handleShowResults() {
@@ -138,8 +155,39 @@ function handleShowResults() {
     resultsButton.removeEventListener('click', handleShowResults);
   }
 }
-
 // ***** EXECTUABLE CODE ******
+
+// *** LOCALS STORAGE CONTINUES ****
+// Step 3 - Get info from local storage
+
+let retrievedItems = localStorage.getItem('myItems');
+
+console.log('Items from local storage >>', retrievedItems);
+
+// STEP 4 - CONVERT BACK TO USABLE CODE
+let parsedItems = JSON.parse(retrievedItems);
+
+console.log('Parsed Items >>>>', parsedItems);
+
+
+// *** REBUILD Goats USING THE Constructor - the hard way *****
+
+if(retrievedItems){
+  for(let i = 0; i < parsedItems.length; i++){
+    if(parsedItems[i].name === 'sweep'){
+      let reconstructedSweep = new Item(parsedItems[i].name, 'png');
+      reconstructedSweep.views = parsedItems[i].views;
+      reconstructedSweep.votes = parsedItems[i].votes;
+      itemArray.push(reconstructedSweep);
+    } else {
+      let reconstructedItem = new Item(parsedItems[i].name);
+      reconstructedItem.views = parsedItems[i].views;
+      reconstructedItem.votes = parsedItems[i].votes;
+      itemArray.push(reconstructedItem);
+    }
+  }
+
+} else {
 let bagItem = new Item('bag');
 let bananaItem = new Item('banana');
 let bathroomItem = new Item('bathroom');
@@ -162,6 +210,35 @@ let wineglassItem = new Item('wine-glass');
 
 itemArray.push(bagItem, bananaItem, bathroomItem, bootsItem, breakfastItem, bubblegumItem, chairItem, cthulhuItem, dogduckItem, dragonItem, penItem, petsweepItem, scissorsItem, sharkItem, sweepItem, tauntaunItem, unicornItem, watercanItem, wineglassItem);
 console.log(itemArray);
+}
+// //*** Happy Path ***
+
+// if (retrievedItems){
+//   itemArray = parsedItems;
+//  } else {
+//     let bagItem = new Item('bag');
+//   let bananaItem = new Item('banana');
+//   let bathroomItem = new Item('bathroom');
+//   let bootsItem = new Item('boots');
+//   let breakfastItem = new Item('breakfast');
+//   let bubblegumItem = new Item('bubblegum');
+//   let chairItem = new Item('chair');
+//   let cthulhuItem = new Item('cthulhu');
+//   let dogduckItem = new Item('dog-duck');
+//   let dragonItem = new Item('dragon');
+//   let penItem = new Item('pen');
+//   let petsweepItem = new Item('pet-sweep');
+//   let scissorsItem = new Item('scissors');
+//   let sharkItem = new Item('shark');
+//   let sweepItem = new Item('sweep', 'png');
+//   let tauntaunItem = new Item('tauntaun');
+//   let unicornItem = new Item('unicorn');
+//   let watercanItem = new Item('water-can');
+//   let wineglassItem = new Item('wine-glass');
+
+// itemArray.push(bagItem, bananaItem, bathroomItem, bootsItem, breakfastItem, bubblegumItem, chairItem, cthulhuItem, dogduckItem, dragonItem, penItem, petsweepItem, scissorsItem, sharkItem, sweepItem, tauntaunItem, unicornItem, watercanItem, wineglassItem);
+// console.log(itemArray);
+// }
 
 renderImg();
 
